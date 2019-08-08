7283b0f80e48f4aae9f8899cc48f756affb78faa
Pavel Antropov <sidexrus@yandex.ru>
2019-08-08 12:25:43 +0700

    Updated SequenceFinder.java, now its working with cycles in dependencies

--- serverD/src/main/java/com/practice/App.java
+++ serverD/src/main/java/com/practice/App.java
@@ -12,9 +12,9 @@ public class App {
         System.out.println( "Hello World!" );
         DBConnection conn = new DBConnection("bolt://localhost:7687",
                 "neo4j", "antropik2");
-        String app_json = "{\"applications\":[\"lel\",\"HelloWorld\"]}";
+        String app_json = "{\"applications\":[\"1\", \"App8\", \"App10\", \"App6\"]}";
         ArrayList<String> apps = AppListWorker.ToArrayList(app_json);
-        ArrayList res = SequenceFinder.FindSequence(apps, conn);
+        ArrayList res = SequenceFinder.FindSequenceWithCycles(apps, conn);
         String sequence = AppListWorker.ToJson(res);
     }
 }
